a5a2b07f717725d7db8cc26541adfcc0244b46d5
oulad <yamghar@coefficy;com>
2022-04-04 19:28:48 +0200

    signUpPage

---
+++ src/components/PersonalInfo.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function PersonalInfo({ formData, setFormData }) {
+  return (
+    <div className="personal-info-container">
+      <input
+        type="text"
+        placeholder="Prénom..."
+        value={formData.firstName}
+        onChange={(e) => {
+          setFormData({ ...formData, firstName: e.target.value });
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Nom de famille..."
+        value={formData.lastName}
+        onChange={(e) => {
+          setFormData({ ...formData, lastName: e.target.value });
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Téléphone..."
+        value={formData.username}
+        onChange={(e) => {
+          setFormData({ ...formData, username: e.target.value });
+        }}
+      />
+    </div>
+  );
+}
+
+export default PersonalInfo;
